b41f09f3b7e0b67a4806f8101ebc15808c99d018
Timothy Woodby <timwoodby@gmail.com>
2022-07-19 11:59:33 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog"
+  const likes = 50
+
   return (
     <div className="App">
       <div  className='content'>
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Likes {likes} times</p>
       </div>
     </div>
   );
