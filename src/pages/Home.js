25cb1a5b63a4dd3cddec9bab0c14b72ec6189186
Timothy Woodby <timwoodby@gmail.com>
2022-07-19 16:56:04 -0400

    udpated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
+import { useState } from "react"
+import BlogList from "../components/BlogList";
+
 const HomePage = () => {
+    const [blogs, setBlogs] = useState([
+        {title: "A blog about all kinds of stuff", body: "A blog of stuff", author: "frank", id: 1},
+        {title: "A blog about data integrity", body: "A blog of data", author: "petter", id: 2},
+        {title: "A blog about nothing", body: "A blog of nothing", author: "sally", id: 3},
+    ]);
+
+
     return (
         <div className="HomePage">
-            <h2>Home Page</h2>
+            <BlogList />
         </div>
     )
-}
+} 
 
 export default HomePage
